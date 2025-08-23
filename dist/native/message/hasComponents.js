1d8700c4eb9f85d8a4009e9c475a6f1d29e385a2
xNickyDev <xnickydev@gmail.com>
2025-08-23 16:12:12 +0200

    Added $arrayUnique and $hasComponents

---
+++ dist/native/message/hasComponents.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const structures_1 = require("../../structures");
+exports.default = new structures_1.NativeFunction({
+    name: "$hasComponents",
+    description: "Checks whether given message has components",
+    brackets: false,
+    unwrap: true,
+    args: [
+        {
+            name: "channel ID",
+            description: "The channel to get message from",
+            type: structures_1.ArgType.Channel,
+            rest: false,
+            required: true,
+            check: (i) => "messages" in i
+        },
+        {
+            name: "message ID",
+            pointer: 0,
+            rest: false,
+            required: true,
+            type: structures_1.ArgType.Message,
+            description: "The message to check for components"
+        }
+    ],
+    output: structures_1.ArgType.Boolean,
+    execute(ctx, [, msg]) {
+        return this.success(!!(msg ?? ctx.message)?.components.length);
+    },
+});
+//# sourceMappingURL=hasComponents.js.map
---
+++ src/native/message/hasComponents.ts
@@ -0,0 +1,32 @@
+import { BaseChannel } from "discord.js"
+import { ArgType, NativeFunction, Return } from "../../structures"
+
+export default new NativeFunction({
+    name: "$hasComponents",
+    version: "2.5.0",
+    description: "Checks whether given message has components",
+    brackets: false,
+    unwrap: true,
+    args: [
+        {
+            name: "channel ID",
+            description: "The channel to get message from",
+            type: ArgType.Channel,
+            rest: false,
+            required: true,
+            check: (i: BaseChannel) => "messages" in i
+        },
+        {
+            name: "message ID",
+            pointer: 0,
+            rest: false,
+            required: true,
+            type: ArgType.Message,
+            description: "The message to check for components"
+        }
+    ],
+    output: ArgType.Boolean,
+    execute(ctx, [, msg]) {
+        return this.success(!!(msg ?? ctx.message)?.components.length)
+    },
+})
